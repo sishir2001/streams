23975377a20e2d076639900d358b62a7fc1329e8
Jaladi Saai Sishir <54372985+sishir2001@users.noreply.github.com>
2022-02-02 15:41:06 +0530

    add: rtmp server

--- client/src/components/streams/StreamShow.js
+++ client/src/components/streams/StreamShow.js
@@ -1,15 +1,55 @@
 import React,{Component} from "react";
 import {connect} from "react-redux";
+import flv from "flv.js";
 
 import {fetchStream} from "../../actions";
 class StreamShow extends Component{
 
+    // creating a reference
+    constructor(props){
+        super(props);
+        this.videoRef = React.createRef();
+    }
+
     componentDidMount(){
         // TODO : call fetchStream action creator
+        const {id} = this.props.match.params;
         // fetches the stream data based on id and updates the state
-        this.props.fetchStream(this.props.match.params.id);
+        this.props.fetchStream(id);
         // console.log(this.props.match.params.id);
+        this.buildPlayer();// this will only run once
+
     }
+
+    // this lifecycle method will be called everytime after component rendering
+    componentDidUpdate(){
+        this.buildPlayer();
+    }
+    componentWillUnmount(){
+        // clean up the player resources 
+        // no longer attempting to download the video
+        this.player.destroy();
+    }
+
+    buildPlayer(){
+        if(this.player || !this.props.stream){
+            return;
+        }
+
+        // only if the stream data is available or player is not built
+        const {id} = this.props.match.params;
+        // this lifecycle method will be called after the render method for once , so this is the place to setup the flv player
+        // ? class variable
+        this.player = flv.createPlayer({
+            type:'flv',
+            url:`http://localhost:8000/live/${id}.flv`
+        });
+        this.player.attachMediaElement(this.videoRef.current);
+        this.player.load();
+        // let the user play the video
+    }
+
+
 
     render(){
         if(!this.props.stream){
@@ -20,6 +60,7 @@ class StreamShow extends Component{
         const {title,description} = this.props.stream;
         return (
             <div>
+                <video ref={this.videoRef} controls style={{width:'100%'}}></video>
                 <h1>{title}</h1>
                 <h5>{description}</h5>
             </div>
@@ -33,6 +74,5 @@ const mapStateToProps = (state,componentProps)=>{
         stream:state.streams[componentProps.match.params.id]
     };
 };
-
 
 export default connect(mapStateToProps,{fetchStream})(StreamShow);
